1fe415f4675fcc795a50e8ae6fdf923c43f1f664
KebabRonin <alrx.brss@gmail.com>
2026-01-29 03:06:33 +0200

    try

--- Types/PCAP/src/PayloadParsers/FtpParser.hpp
+++ Types/PCAP/src/PayloadParsers/FtpParser.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "API.hpp"
+#include "PCAP.hpp"
 
 namespace GView::Type::PCAP::Panels
 {
@@ -20,12 +21,14 @@ struct FtpSession {
     bool expectingPassword = false;
 };
 struct FTPParser : public PayloadDataParserInterface {
+    PCAPFile& pfile;
+    FTPParser(PCAPFile& _pfile) : pfile(_pfile) {};
     std::string GetProtocolName() const override
     {
         return "FTP";
     }
 
     PayloadDataParserInterface* ParsePayload(const PayloadInformation& payloadInformation, ConnectionCallbackInterface* callbackInterface) override;
-    void HandleCommand(const std::string& line, ConnectionCallbackInterface* cb);
+    void HandleCommand(const std::string& line, Panels::FTP_PANEL_SUMMARY_LINES_TYPE& layerSummaryPanelLines);
 };
 } // namespace GView::Type::PCAP::FTP
